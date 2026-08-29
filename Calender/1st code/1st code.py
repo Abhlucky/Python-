4bfe9c895ebcc438095ecca4cb2a8cac5ab8cd91
Abhay Kumar <271042178+Abhlucky@users.noreply.github.com>
2026-08-29 18:23:51 +0530

    Add files via upload

---
+++ Calender/1st code/1st code.py
@@ -0,0 +1,22 @@
+print ("hello world")
+print (3*55-44/2)
+print("3*44+6-5")
+print (8/2)
+print (8//2)
+print (type(88))
+print (type("88"))
+x = 9
+y = 8
+print(x + y)
+print ("Hello world")
+print ("89+1")
+print (89+1)
+print ('''I'm here where are you''')
+print (8*4/6+5-10)
+print (6/3)
+print (6//3)
+print (type (77))
+print (type ("66"))
+x = 53
+y = 2
+print(x - y)
